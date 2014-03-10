a97933fefdf7bca08f26284790ac6110aa397479
jsakamoto <junichi.jp@gmail.com>
2014-03-10 23:14:49 +0900

    Fix Package: Repackage with Release build. (1.0.5 was Debug build)

--- IPAddressRange/Properties/AssemblyInfo.cs
+++ IPAddressRange/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("IPAddressRange")]
-[assembly: AssemblyCopyright("Copyright © 2012-2013 J.Sakamoto, Ms-PL License.")]
+[assembly: AssemblyCopyright("Copyright © 2012-2014 J.Sakamoto, Ms-PL License.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
