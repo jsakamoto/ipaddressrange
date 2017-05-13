51f3450311d6c0b755a77531928c1c89d14a116c
jsakamoto <junichi.jp@gmail.com>
2017-05-13 14:49:41 +0900

    [BREAKING CHANGE] Truncate .NET Framework v.4.0 support
    - Instead, improve JSON converting with JSON.NET.

--- IPAddressRange/IPAddressRange.cs
+++ IPAddressRange/IPAddressRange.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using System.Text.RegularExpressions;
+using System.ComponentModel;
 
 #if IPADDRESSRANGE_NETFX45
 using System.Runtime.Serialization;
@@ -12,9 +14,9 @@ namespace NetTools
 {
 #if IPADDRESSRANGE_NETFX45
     [Serializable]
-    public class IPAddressRange : ISerializable, IEnumerable<IPAddress>
+    public class IPAddressRange : ISerializable, IEnumerable<IPAddress>, IReadOnlyDictionary<string, string>
 #else
-    public class IPAddressRange : IEnumerable<IPAddress>
+    public class IPAddressRange : IEnumerable<IPAddress>, IReadOnlyDictionary<string, string>
 #endif
     {
         // Pattern 1. CIDR range: "192.168.0.0/24", "fe80::/10"
@@ -283,5 +285,53 @@ namespace NetTools
         {
             return string.Format("{0}/{1}", Begin, GetPrefixLength());
         }
+
+        #region JSON.NET Support by implement IReadOnlyDictionary<string, string>
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public IPAddressRange(IEnumerable<KeyValuePair<string, string>> items)
+        {
+            this.Begin = IPAddress.Parse(TryGetValue(items, nameof(Begin), out var value1) ? value1 : throw new KeyNotFoundException());
+            this.End = IPAddress.Parse(TryGetValue(items, nameof(End), out var value2) ? value2 : throw new KeyNotFoundException());
+        }
+
+        /// <summary>
+        /// Returns the input typed as IEnumerable&lt;IPAddress&gt;
+        /// </summary>
+        public IEnumerable<IPAddress> AsEnumerable() => (this as IEnumerable<IPAddress>);
+
+        private IEnumerable<KeyValuePair<string, string>> GetDictionaryItems()
+        {
+            return new[] {
+                new KeyValuePair<string,string>(nameof(Begin), Begin.ToString()),
+                new KeyValuePair<string,string>(nameof(End), End.ToString()),
+            };
+        }
+
+        private bool TryGetValue(string key, out string value) => TryGetValue(GetDictionaryItems(), key, out value);
+
+        private bool TryGetValue(IEnumerable<KeyValuePair<string, string>> items, string key, out string value)
+        {
+            items = items ?? GetDictionaryItems();
+            var foundItem = items.FirstOrDefault(item => item.Key == key);
+            value = foundItem.Value;
+            return foundItem.Key != null;
+        }
+
+        IEnumerable<string> IReadOnlyDictionary<string, string>.Keys => GetDictionaryItems().Select(item => item.Key);
+
+        IEnumerable<string> IReadOnlyDictionary<string, string>.Values => GetDictionaryItems().Select(item => item.Value);
+
+        int IReadOnlyCollection<KeyValuePair<string, string>>.Count => GetDictionaryItems().Count();
+
+        string IReadOnlyDictionary<string, string>.this[string key] => TryGetValue(key, out var value) ? value : throw new KeyNotFoundException();
+
+        bool IReadOnlyDictionary<string, string>.ContainsKey(string key) => GetDictionaryItems().Any(item => item.Key == key);
+
+        bool IReadOnlyDictionary<string, string>.TryGetValue(string key, out string value) => TryGetValue(key, out value);
+
+        IEnumerator<KeyValuePair<string, string>> IEnumerable<KeyValuePair<string, string>>.GetEnumerator() => GetDictionaryItems().GetEnumerator();
+
+        #endregion
     }
 }
