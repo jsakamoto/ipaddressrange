9764fa73fe74533a05e0ac166b15d8e5613fbf10
jsakamoto <junichi.jp@gmail.com>
2018-01-29 16:02:30 +0900

    v.2.2.0 release

--- IPAddressRange/Properties/AssemblyInfo.cs
+++ IPAddressRange/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("IPAddressRange")]
-[assembly: AssemblyCopyright("Copyright © 2012-2017 J.Sakamoto, Mozilla Public License 2.0")]
+[assembly: AssemblyCopyright("Copyright © 2012-2018 J.Sakamoto, Mozilla Public License 2.0")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
