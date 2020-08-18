9c8750c3ff630b57a94a0256d57b3c753cf147cb
jsakamoto <junichi.jp@gmail.com>
2020-08-18 12:18:42 +0900

    Add benchmark

---
+++ IPAddressRange.Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace NetTools.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<IPv4EnumerateTest>();
+        }
+    }
+}
